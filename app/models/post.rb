a969d6dff609c20f1384de2a347d4e27e23aa25c
Connor Meth <connormeth27@gmail.com>
2019-10-16 14:08:03 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    belongs_to :author
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(Non-Fiction Fiction),
+    message: "%{value} is not a valid category" }
+    validates :content, length: { minimum: 100 }
 end
